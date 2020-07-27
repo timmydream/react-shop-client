66a09570886b84f95ad529853f33af02277b0979
Timur Demenov <49468087+timmydream@users.noreply.github.com>
2020-07-27 20:09:04 +0600

    added heroku's link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000';
+const url = 'https://sleepy-chamber-68489.herokuapp.com';
 
 export const Axios = {
   fetchItems: () => axios.get(`${url}/items`),
